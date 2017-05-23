2e0686d033e9981d93e6d4f952a19c3afcd9eaa4
clingoram <llingchen.27@gmail.com>
2017-05-23 16:05:32 +0800

    Create script.js

---
+++ answers/hw4/js/script.js
@@ -0,0 +1,34 @@
+$(function() {
+
+    console.log('JavaScript is loading...');
+
+    function getData() {
+
+        $.ajax({
+            type: 'GET',
+            url: 'https://api.twitch.tv/kraken/streams/?game=League%20of%20Legends',
+            headers: {
+                'Accept': 'application/vnd.twitchtv.v5+json',
+                'Client-ID': 'rx55ypcn5s81meroo6b5fagl1bwwkw' // need to register a 'developer application' on the connections page of Twitch account.
+            },
+
+            success: function(data) {
+
+                console.log('ajax is loading successfully!!');
+
+                for (var i = 0; i <= 20; i++) {
+                    // iframe
+                    $('.video_' + i).attr('src', 'https://player.twitch.tv/?autoplay=false&channel=' + data.streams[i].channel.name);
+                    // host icon
+                    $('.img_' + i).attr('src', data.streams[i].channel.logo);
+                    // status of channel
+                    $('#info_' + i).html('<p class="channel">' + data.streams[i].channel.status + '</p><p class="host">' + data.streams[i].channel.display_name + '</p>');
+                }
+            },
+            error: function(err) {
+                console.log('Error');
+            }
+        });
+    }
+getData();
+})
