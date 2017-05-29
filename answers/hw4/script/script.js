8d355788a908ab323dbeccd4aeccfb6a105ece00
clingoram <llingchen.27@gmail.com>
2017-05-29 14:21:09 +0800

    Update script.js

--- answers/hw4/script/script.js
+++ answers/hw4/script/script.js
@@ -2,34 +2,46 @@ $(function() {
 
     console.log('JavaScript is loading...');
 
+    var twitchURL = 'https://api.twitch.tv/kraken/streams/?game=League%20of%20Legends';
+    var myContentType = 'application/vnd.twitchtv.v5+json';
+    var clientID = 'rx55ypcn5s81meroo6b5fagl1bwwkw';
+
     function getData() {
 
         $.ajax({
             type: 'GET',
-            url: 'https://api.twitch.tv/kraken/streams/?game=League%20of%20Legends',
+            url: twitchURL,
             headers: {
-                'Accept': 'application/vnd.twitchtv.v5+json',
-                'Client-ID': 'rx55ypcn5s81meroo6b5fagl1bwwkw' // need to register a 'developer application' on the connections page of Twitch account.
+                'Accept': myContentType,
+                'Client-ID': clientID // need to register a 'developer application' on the connections page of Twitch account.
             },
 
             success: function(data) {
 
                 console.log('ajax is loading successfully!!');
-                var autoplay= false;
-                
-                for (var i = 0; i <=20; i++) {
-                    // iframe
-                    $('.video_' + i).attr('src', 'https://player.twitch.tv/?autoplay='+ autoplay +'&channel=' + data.streams[i].channel.name);
-                    // host icon
-                    $('.img_' + i).attr('src', data.streams[i].channel.logo);
-                    // status of channel
-                    $('#info_' + i).html('<p class="channel">' + data.streams[i].channel.status + '</p><p class="host">' + data.streams[i].channel.display_name + '</p>');
+
+                for (var i = 0; i <= 20; i++) {
+                    var $content =`
+                    <li class='item'>
+                        <div class='video_'>
+                            <img class='placeholder' src='${data.streams[i].preview.medium}' />
+                        </div>
+                        <div class='avatar'> 
+                            <img class='img_' src='${data.streams[i].channel.logo}'>
+                            <div id='info_' class='info'> 
+                                <p class='channel'> ${data.streams[i].channel.status} </p>  
+                                <p class='host'> ${data.streams[i].channel.display_name} </p>
+                            </div>
+                        </div>
+                    </li>`;
+                    $( '#content').append($content);
+                    $content = $();
                 }
             },
-            error: function() {
-                console.log('Error');
+            error: function(data) {
+                console.log('Error',data);
             }
         });
     }
-getData();
+    getData();
 })
