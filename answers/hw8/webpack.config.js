bf54f5ab131c32eb0c62cdb455fbbbedb4890135
clingoram <llingchen.27@gmail.com>
2017-06-05 08:32:31 +0800

    Create webpack.config.js

---
+++ answers/hw8/webpack.config.js
@@ -0,0 +1,29 @@
+var webpack = require('webpack');
+var path = require('path');
+
+module.exports = {
+    // 程式的入口
+    entry: './app/index.js',
+    // 要輸出到哪裡
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    plugins: [
+        // ProvidePlugin
+        new webpack.ProvidePlugin({
+            $: "jquery",
+            jQuery: "jquery"
+        })
+    ],
+
+    // 載入哪些類型的檔案 
+    module: {
+
+        // loader:指定要載入的loader
+        loaders: [
+            { test: /\.css$/, loader: ['style-loader', 'css-loader'] }, // to load css
+            { test: /\.(png|jpg)$/, loader: 'url-loader' } // to laod img
+        ],
+    }
+}
