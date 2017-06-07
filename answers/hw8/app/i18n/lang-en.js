32f808965d36df1c025edbe36516be81d7a7880a
clingoram <llingchen.27@gmail.com>
2017-06-07 12:56:30 +0800

    Update lang-en.js

--- answers/hw8/app/i18n/lang-en.js
+++ answers/hw8/app/i18n/lang-en.js
@@ -1,5 +1,4 @@
-if (!window.I18N) window.I18N = {};
-window.I18N['en'] = {
-  TITLE: 'The Streams in English.',
-  WORDS:'English(US).'
+module.exports = {
+	TITLE:'The Streams in English.',
+    WORDS:'English(US).'
 }
